b8b629d759420dfcf2ba3139ca9480e76bf1a4d8
Sayyid Abbas <sayyid.abbas.ar@gmail.com>
2025-07-07 11:30:09 +0300

    Adding `Button To Scroll Up` For The First Three Projects

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Displaying the menu of links on click
+
 let links = document.querySelector(".links");
 
 links.onclick = function () {
@@ -9,3 +11,15 @@ document.addEventListener("click", function (e) {
         links.classList.remove("show");
     }
 });
+
+// A button to scroll up
+
+let myButton = document.querySelector(".btn");
+window.onscroll = function() {
+    if(window.scrollY < 1000) {
+        myButton.classList.remove("show");
+    } else {
+        myButton.classList.add("show");
+    }
+}
+myButton.onclick = () => window.scrollTo({top: 0, behavior: "smooth"});
